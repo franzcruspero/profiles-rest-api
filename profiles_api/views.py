6e32a636e85a5b0f3a3156d726ff301bec88c216
Franz Andrei M. Cruspero <franz.cruspero@gmail.com>
2020-05-22 17:49:03 +0800

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,8 @@ from rest_framework import status, viewsets, filters
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers, models, permissions
 
@@ -59,7 +61,7 @@ class HelloApiView(APIView):
 
 
 class HelloViewSet(viewsets.ViewSet):
-    """Test API Viewset"""
+    """Test API endpoint Viewset"""
 
     serializer_class = serializers.HelloSerializer
 
@@ -114,3 +116,8 @@ class UserProfileViewset(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
